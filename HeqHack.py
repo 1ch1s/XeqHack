66a99c5b8b5a04c6cf50632d81b99d6e76fab75d
1ch1s <buchelnikov.alexeii@gmail.com>
2024-12-12 16:29:30 +0500

    HeqHack.py

--- HeqHack.py
+++ HeqHack.py
@@ -7,6 +7,8 @@ igrok = pymem.process.module_from_name(sperma.process_handle, "client.dll")
 dwLocalPlayerPawn = sperma.read_longlong(igrok.lpBaseOfDll + 0x1855CE8)
 dwViewRender = sperma.read_longlong(igrok.lpBaseOfDll + 0x1A54D60)
 dwLocalPlayerController = sperma.read_longlong(igrok.lpBaseOfDll + 0x1A41FD0)
+dwEntityList = sperma.read_longlong(igrok.lpBaseOfDll + 0x19F2488)
+dwGlowManager = sperma.read_longlong(igrok.lpBaseOfDll + 0x1A4F5A8)
 
 sperma.write_int(dwLocalPlayerController + 0x6F4, 90)
 sperma.write_float(dwLocalPlayerPawn + 0x1408, 1.0)
