50cb72e50e803b2f75abd4f9241817a6311e6051
1ch1s <166480725+1ch1s@users.noreply.github.com>
2024-12-12 16:21:21 +0500

    Add files via upload

---
+++ HeqHack.py
@@ -0,0 +1,14 @@
+import pymem
+import pymem.process
+
+sperma = pymem.Pymem("cs2.exe")
+igrok = pymem.process.module_from_name(sperma.process_handle, "client.dll")
+
+dwLocalPlayerPawn = sperma.read_longlong(igrok.lpBaseOfDll + 0x1855CE8)
+dwViewRender = sperma.read_longlong(igrok.lpBaseOfDll + 0x1A54D60)
+dwLocalPlayerController = sperma.read_longlong(igrok.lpBaseOfDll + 0x1A41FD0)
+
+sperma.write_int(dwLocalPlayerController + 0x6F4, 90)
+sperma.write_float(dwLocalPlayerPawn + 0x1408, 1.0)
+sperma.write_float(dwLocalPlayerPawn + 0x14B0, 1.0)
+sperma.write_float(dwLocalPlayerPawn + 0x14B4, 1.0)
