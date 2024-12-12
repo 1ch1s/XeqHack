41f7d46ee7f414c45e6f73621d3f6dc2ac7f9542
1ch1s <buchelnikov.alexeii@gmail.com>
2024-12-12 16:37:36 +0500

    HeqHack.py

--- HeqHack.py
+++ HeqHack.py
@@ -1,16 +1,16 @@
 import pymem
 import pymem.process
 
-sperma = pymem.Pymem("cs2.exe")
-igrok = pymem.process.module_from_name(sperma.process_handle, "client.dll")
+memory = pymem.Pymem("cs2.exe")
+player = pymem.process.module_from_name(memory.process_handle, "client.dll")
 
-dwLocalPlayerPawn = sperma.read_longlong(igrok.lpBaseOfDll + 0x1855CE8)
-dwViewRender = sperma.read_longlong(igrok.lpBaseOfDll + 0x1A54D60)
-dwLocalPlayerController = sperma.read_longlong(igrok.lpBaseOfDll + 0x1A41FD0)
-dwEntityList = sperma.read_longlong(igrok.lpBaseOfDll + 0x19F2488)
-dwGlowManager = sperma.read_longlong(igrok.lpBaseOfDll + 0x1A4F5A8)
+dwLocalPlayerPawn = memory.read_longlong(player.lpBaseOfDll + 0x1855CE8)
+dwViewRender = memory.read_longlong(player.lpBaseOfDll + 0x1A54D60)
+dwLocalPlayerController = memory.read_longlong(player.lpBaseOfDll + 0x1A41FD0)
+dwEntityList = memory.read_longlong(player.lpBaseOfDll + 0x19F2488)
+dwGlowManager = memory.read_longlong(player.lpBaseOfDll + 0x1A4F5A8)
 
-sperma.write_int(dwLocalPlayerController + 0x6F4, 90)
-sperma.write_float(dwLocalPlayerPawn + 0x1408, 1.0)
-sperma.write_float(dwLocalPlayerPawn + 0x14B0, 1.0)
-sperma.write_float(dwLocalPlayerPawn + 0x14B4, 1.0)
+memory.write_int(dwLocalPlayerController + 0x6F4, 90)
+memory.write_float(dwLocalPlayerPawn + 0x1408, 1.0)
+memory.write_float(dwLocalPlayerPawn + 0x14B0, 1.0)
+memory.write_float(dwLocalPlayerPawn + 0x14B4, 1.0)
